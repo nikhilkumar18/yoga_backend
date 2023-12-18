f931fd9021333f23a5dcfe5a7faf21b264f3341a
nikhilkumar18 <104351104+nikhilkumar18@users.noreply.github.com>
2023-12-18 14:57:51 +0530

    Update urls.py

--- yoga_backend/urls.py
+++ yoga_backend/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [ 
     path("admin/", admin.site.urls),
-    path('', include('yoga_api.urls')),
+    path('index/', include('yoga_api.urls')),
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
